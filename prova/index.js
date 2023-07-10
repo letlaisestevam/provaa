60695e6743ec8f6e9f65b4578b433a1f7521c30c
Letícia Laís <110117716+letlaisestevam@users.noreply.github.com>
2023-07-10 18:24:08 -0300

    Add files via upload

---
+++ prova/index.js
@@ -0,0 +1,52 @@
+//TAREFA 1
+//const http = require ('http')
+//const fs = require('fs')
+//const os = require('os')
+//const port = 3000
+//
+//console.log(os.freemem())
+//var memo = os.freemem / 1000
+//console.log(os.type())
+//console.log(`${memo} KB`)
+//
+//console.log(os.homedir())
+//
+//const server = http.createServer((req, res) =>{
+//    res.setHeader(200,{ 'Content-Type': 'application/json'})
+//    res.end('yeah')
+//    res.write(data)
+//    return res.end()
+//}) 
+//server.listen(port, () =>{
+//    console.log(`Servidor massa demais ╰(*°▽°*)╯ ${port}`)
+//})
+
+//TAREFA 2
+//
+//const path = require('path')
+//const repositorio = 'https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pd
+//f'
+//const rota = {
+   // root: '',
+    //dir:
+//}
+
+//TAREFA 3
+//
+
+
+
+//TAREFA 4
+//
+//const fs = require('fs')
+//
+//fs.rename('uipapito.txt', 'papito.txt', function (err, data){
+//    if (err) throw (err)
+//    console.log('Arquivo Renomeado ᓚᘏᗢ ')
+//})
+
+//TAREFA 5
+//
+//const os = require('os')
+//console.log(os.cpus())
+
